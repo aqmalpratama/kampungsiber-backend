0f51510e544e6075195fba4e10334f840dc6ba34
Muhammad Aqmal Khafidz Pratama <aqmal.pratama81@gmail.com>
2022-12-24 14:56:57 +0700

    UPDATE - Consultation API

--- api/consultationApi.py
+++ api/consultationApi.py
@@ -18,7 +18,7 @@ def consultationMentor():
     try:
         connection = mysql.connect()
         cursor = connection.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM `user` WHERE `reg_type` = 1 and `is_acc_admin` = 1")
+        cursor.execute("SELECT * FROM `user_profile` WHERE `reg_type` = 1 and `is_approved_admin` = 1")
         rows = cursor.fetchall()
         cursor.close()
         connection.close()
@@ -53,6 +53,32 @@ def createConsultationSession():
     except Exception as e:
         print(e)
         response = jsonify({'message' : 'Something went wrong, contact admin'})
+        response.status_code = 400
+    finally:
+        return response
+
+@app.route('/consultation/upcoming/<string:type>/<int:mentor_id>', methods=['GET'])
+def upcomingConsultation(type, mentor_id):
+    try:
+        if type == 'mentor':
+            sql = "select * from vw_consultation_request where mentor_id = %s and cast(concat(consultation_date , ' ', start_time) as datetime) > now() and verify_payment = 1"
+            data = (mentor_id)
+        elif type == 'requestor':
+            sql = "select * from vw_consultation_request where requestor_id = %s and cast(concat(consultation_date , ' ', start_time) as datetime) > now() and verify_payment = 1"
+            data = (mentor_id)
+        connection = mysql.connect()
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql, data)
+        rows = cursor.fetchall()
+        if rows:
+            response = jsonify(rows)
+            response.status_code = 200
+        else:
+            response = jsonify({'message' : 'No upcoming consultation'})
+            response.status_code = 400
+    except Exception as e:
+        print(e)
+        response = jsonify( {'message' : 'Something went wrong, contact admin'})
         response.status_code = 400
     finally:
         return response
